cc24d918221ad9c55f122689eb3bbb4ef485ae48
Esther Okiki <estherokiki15@gmail.com>
2024-04-23 02:31:19 +0100

    Upload File

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,30 +172,7 @@ containerHovers.forEach(containerHover => {
 });
 
 
-// //FOR  MOBILE
-// containerHovers.forEach(containerHover => {
-//     containerHover.addEventListener('touchstart', function () {
-//         // Select the image within the current container being hovered over
-//         const containerImgs = containerHover.querySelector('.menus-container_item--img');
-//         const containerImgsImg = containerHover.querySelector('.menus-container_item--img-img');
-
-//         containerImgs.classList.add('menu-hover');
-//         containerImgsImg.classList.add('menu-hover2');
-//     });
-
-//     containerHover.addEventListener('touchend', function () {
-//         // Select the image within the current container being hovered over
-//         const containerImgs = containerHover.querySelector('.menus-container_item--img');
-//         const containerImgsImg = containerHover.querySelector('.menus-container_item--img-img');
-
-//         containerImgs.classList.remove('menu-hover');
-//         containerImgsImg.classList.remove('menu-hover2');
-//     });
-// });
-
-
 /*
-
 const AddMenuClass = function (currentContainer) {
     // Select the image within the current container being hovered over
     const containerImgs = containerHover.querySelector('.menus-container_item--img');
