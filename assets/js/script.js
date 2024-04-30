945d6217b555de98aa1dc38f081478f58dd39757
Esther Okiki <estherokiki15@gmail.com>
2024-04-30 14:32:11 +0100

    Upload File

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,30 @@
-
 "use strict";
 // const preLoaderEl = document.querySelector('.preloader');
-const landingPageEl = document.querySelector('.landingPage');
+const landingPageEl = document.querySelector(".landingPage");
 
-const imgEl = document.querySelector('.heroPage-Img-img');
+const imgEl = document.querySelector(".heroPage-Img-img");
 
-const heroHeadText = document.querySelector('.heroTexts--heading');
+const heroHeadText = document.querySelector(".heroTexts--heading");
 
-const heroCursiveText =  document.querySelector('.heroTexts--cursiveTexts');
+const heroCursiveText = document.querySelector(".heroTexts--cursiveTexts");
 
-const openBtn = document.querySelector('.landingPage-navBar--menuBar_openMenu');
+const openBtn = document.querySelector(".landingPage-navBar--menuBar_openMenu");
 
-const closeBtn = document.querySelector('.navBar-links_heading--close-btn');
+const closeBtn = document.querySelector(".navBar-links_heading--close-btn");
 
-const navQuickLinks = document.querySelector('.navBar-links');
+const navQuickLinks = document.querySelector(".navBar-links");
 
-const navQuickLinksUl = document.querySelector('.navbar-link-item--ul');
+const navQuickLinksUl = document.querySelector(".navbar-link-item--ul");
 
-const heroHomePage = document.querySelector('.hero-home-page')
+const heroHomePage = document.querySelector(".hero-home-page");
 
-const arrHead1 = document.querySelector('.desktopArrHead-1');
+const arrHead1 = document.querySelector(".desktopArrHead-1");
 
-const arrHead2 = document.querySelector('.desktopArrHead-2');
+const arrHead2 = document.querySelector(".desktopArrHead-2");
 
-const fixedArr = document.querySelector('.fixed-arrow-btn');
+const fixedArr = document.querySelector(".fixed-arrow-btn");
 
-const preloader = document.querySelector('.preloader');
-
+const preloader = document.querySelector(".preloader");
 
 //PRELOADER
 // window.addEventListener('load', ()=>{
@@ -34,98 +32,95 @@ const preloader = document.querySelector('.preloader');
 //     // preloader.style.zIndex = -1;
 // });
 
-
 // HERO PAGE
 const imgUrls = [
-    './assets/images/hero-slider-1.jpg',
-    './assets/images/hero-slider-2.jpg',
-    './assets/images/hero-slider-3.jpg'
+  "./assets/images/hero-slider-1.jpg",
+  "./assets/images/hero-slider-2.jpg",
+  "./assets/images/hero-slider-3.jpg",
 ];
 
 function textChanger() {
-    if (imgEl.getAttribute("src") === './assets/images/hero-slider-1.jpg') {
-        heroHeadText.innerHTML = `Traditional Food & Hygiene`;
-        heroCursiveText.innerHTML = `For the Love of <br>
+  if (imgEl.getAttribute("src") === "./assets/images/hero-slider-1.jpg") {
+    heroHeadText.innerHTML = `Traditional Food & Hygiene`;
+    heroCursiveText.innerHTML = `For the Love of <br>
                          Delicious Food`;
-                         
-    }else if(imgEl.getAttribute("src") === './assets/images/hero-slider-2.jpg'){
-        heroHeadText.innerHTML = `Delightful Experience`;
-        heroCursiveText.innerHTML = `Flavours Inspired by </br>
+  } else if (
+    imgEl.getAttribute("src") === "./assets/images/hero-slider-2.jpg"
+  ) {
+    heroHeadText.innerHTML = `Delightful Experience`;
+    heroCursiveText.innerHTML = `Flavours Inspired by </br>
                                  the Seasons`;
-                    
-
-    }else if (imgEl.getAttribute("src") === './assets/images/hero-slider-3.jpg'){
-        heroHeadText.innerHTML = `Amazing & Delicious`;
-        heroCursiveText.innerHTML = `Where Every Flavour </br>
+  } else if (
+    imgEl.getAttribute("src") === "./assets/images/hero-slider-3.jpg"
+  ) {
+    heroHeadText.innerHTML = `Amazing & Delicious`;
+    heroCursiveText.innerHTML = `Where Every Flavour </br>
                            Tells a Story`;
-                         
-    };
+  }
 }
-
 
 let currentIndex = 0;
 
 function changeImg() {
+  imgEl.src = imgUrls[currentIndex];
 
-    imgEl.src = imgUrls[currentIndex];
+  currentIndex = (currentIndex + 1) % imgUrls.length;
+  textChanger();
 
-    currentIndex = (currentIndex + 1) % imgUrls.length;
-    textChanger();
-    
-    setTimeout(changeImg, 7000);
-};
+  setTimeout(changeImg, 7000);
+}
 
 changeImg(); // Initial call to start cycling through images
 
-
 //ARROW FOR CHANGING IMAGE
-arrHead1.addEventListener('click', ()=>{
-   
-    if (imgEl.getAttribute("src") === './assets/images/hero-slider-1.jpg') {
-       imgEl.setAttribute("src", './assets/images/hero-slider-3.jpg');
-       textChanger();
-    
-    }else if (imgEl.getAttribute("src") === './assets/images/hero-slider-3.jpg') {
-        imgEl.setAttribute("src", './assets/images/hero-slider-2.jpg');
-        textChanger();
-    
-    }else if (imgEl.getAttribute("src") === './assets/images/hero-slider-2.jpg') {
-        imgEl.setAttribute("src", './assets/images/hero-slider-1.jpg');
-        textChanger();
-    }
-    });
-
-arrHead2.addEventListener('click', ()=>{
-if (imgEl.getAttribute("src") === './assets/images/hero-slider-1.jpg') {
-   imgEl.setAttribute("src", './assets/images/hero-slider-2.jpg');
-   textChanger();
-
-}else if (imgEl.getAttribute("src") === './assets/images/hero-slider-2.jpg') {
-    imgEl.setAttribute("src", './assets/images/hero-slider-3.jpg');
+arrHead1.addEventListener("click", () => {
+  if (imgEl.getAttribute("src") === "./assets/images/hero-slider-1.jpg") {
+    imgEl.setAttribute("src", "./assets/images/hero-slider-3.jpg");
     textChanger();
-
-}else if (imgEl.getAttribute("src") === './assets/images/hero-slider-3.jpg') {
-    imgEl.setAttribute("src", './assets/images/hero-slider-1.jpg');
+  } else if (
+    imgEl.getAttribute("src") === "./assets/images/hero-slider-3.jpg"
+  ) {
+    imgEl.setAttribute("src", "./assets/images/hero-slider-2.jpg");
     textChanger();
-}
+  } else if (
+    imgEl.getAttribute("src") === "./assets/images/hero-slider-2.jpg"
+  ) {
+    imgEl.setAttribute("src", "./assets/images/hero-slider-1.jpg");
+    textChanger();
+  }
 });
 
+arrHead2.addEventListener("click", () => {
+  if (imgEl.getAttribute("src") === "./assets/images/hero-slider-1.jpg") {
+    imgEl.setAttribute("src", "./assets/images/hero-slider-2.jpg");
+    textChanger();
+  } else if (
+    imgEl.getAttribute("src") === "./assets/images/hero-slider-2.jpg"
+  ) {
+    imgEl.setAttribute("src", "./assets/images/hero-slider-3.jpg");
+    textChanger();
+  } else if (
+    imgEl.getAttribute("src") === "./assets/images/hero-slider-3.jpg"
+  ) {
+    imgEl.setAttribute("src", "./assets/images/hero-slider-1.jpg");
+    textChanger();
+  }
+});
 
 // SLIDING NAV
-openBtn.addEventListener('click', function () {
-    navQuickLinks.classList.add('slidingNav');
+openBtn.addEventListener("click", function () {
+  navQuickLinks.classList.add("slidingNav");
 });
 
-closeBtn.addEventListener('click', function () {
-    navQuickLinks.classList.remove('slidingNav');
+closeBtn.addEventListener("click", function () {
+  navQuickLinks.classList.remove("slidingNav");
 });
-
 
 // HIDE SLIDING NAV BAR
-heroHomePage.addEventListener('click', () =>{
-    navQuickLinks.classList.remove('slidingNav');
-    });
-    
+heroHomePage.addEventListener("click", () => {
+  navQuickLinks.classList.remove("slidingNav");
+});
+
 // FIXED ARROW
 // (function () {
 //   const scrollPosition = 75;
@@ -138,7 +133,6 @@ heroHomePage.addEventListener('click', () =>{
 
 //  })();
 
-
 //  if (landingPageEl) {
 //         setTimeout(() => {
 //            fixedArr.classList.remove('hideFixed');
@@ -146,79 +140,75 @@ heroHomePage.addEventListener('click', () =>{
 //         }, 3000);
 //        }
 
+//OUR MENUS
+const containerHovers = document.querySelectorAll(".menus-container_item");
 
+function handleContainerHover() {
+  const containerImgs = this.querySelector(".menus-container_item--img");
+  const containerImgsImg = this.querySelector(".menus-container_item--img-img");
 
-const containerHovers = document.querySelectorAll('.menus-container_item');
+  containerImgs.classList.toggle("menu-hover");
+  containerImgsImg.classList.toggle("menu-hover2");
+}
 
-containerHovers.forEach(containerHover => {
-    containerHover.addEventListener('mouseover', function () {
-        
-        const containerImgs = containerHover.querySelector('.menus-container_item--img');
-        const containerImgsImg = containerHover.querySelector('.menus-container_item--img-img');
-
-        containerImgs.classList.add('menu-hover');
-        containerImgsImg.classList.add('menu-hover2');
-    });
-
-    containerHover.addEventListener('mouseout', function () {
-      
-        const containerImgs = containerHover.querySelector('.menus-container_item--img');
-        const containerImgsImg = containerHover.querySelector('.menus-container_item--img-img');
-
-        containerImgs.classList.remove('menu-hover');
-        containerImgsImg.classList.remove('menu-hover2');
-    });
+containerHovers.forEach((hover) => {
+  hover.addEventListener("mouseover", handleContainerHover);
+  hover.addEventListener("mouseout", handleContainerHover);
+  hover.addEventListener("touchstart", handleContainerHover);
+  hover.addEventListener("touchend", handleContainerHover);
 });
 
+//FORM DATE
+const currentDate = new Date();
+const locale = navigator.language;
 
-containerHovers.forEach(containerHover => {
-    containerHover.addEventListener('touchstart', function () {
-        
-        const containerImgs = containerHover.querySelector('.menus-container_item--img');
-        const containerImgsImg = containerHover.querySelector('.menus-container_item--img-img');
+const options = {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+};
 
-        containerImgs.classList.add('menu-hover');
-        containerImgsImg.classList.add('menu-hover2');
-    });
+const formattedDate = new Intl.DateTimeFormat(locale, options).format(
+  currentDate
+);
+const [month, day, year] = formattedDate.split("/");
+const formattedDateString = `${year}-${month}-${day}`;
+console.log(formattedDateString);
 
-    containerHover.addEventListener('touchend', function () {
-      
-        const containerImgs = containerHover.querySelector('.menus-container_item--img');
-        const containerImgsImg = containerHover.querySelector('.menus-container_item--img-img');
+document.getElementById("reservation-date").value = formattedDateString;
 
-        containerImgs.classList.remove('menu-hover');
-        containerImgsImg.classList.remove('menu-hover2');
-    });
+//OUR STRENGTH
+const strengthHover = document.querySelectorAll(".strength--container__card");
+
+function handleStrengthHover() {
+  const strengthContainer = this.querySelector(
+    ".strength--container__card-img img"
+  );
+  strengthContainer.classList.toggle("rollImage");
+}
+
+strengthHover.forEach((card) => {
+  card.addEventListener("mouseover", handleStrengthHover);
+  card.addEventListener("mouseout", handleStrengthHover);
+  card.addEventListener("touchstart", handleStrengthHover);
+  card.addEventListener("touchend", handleStrengthHover);
 });
 
+//EVENT PROPAGATION
+// const strengthContainer = document.getElementById('strengthContainer');
 
+// function handleHover(e) {
+//     const target = e.target;
 
-//prevent menu link default
+//     // Check if the target element is a card
+//     if (target.classList.contains('strength--container__card')) {
+//         const strengthContainer = target.querySelector('.strength--container__card-img img');
+//         strengthContainer.classList.toggle('rollImage');
+//     }
+// }
 
-
-
-// containerImgs.forEach(containerImg => {
-//     containerImg.addEventListener('mouseenter', function () {
-//         console.log(100);
-//     });
-// });
-
-
-
-// const quiz1 = 1 + '2' + 3 - '4' + 5;
-// const quiz2 = (1 + '2' + 3) - '4' + 5;
-
-// console.log(quiz1);
-// console.log(quiz2);
-
-
-// Get the current date
-var currentDate = new Date();
-
-// Format the date as "yyyy-MM-dd" which is the required format for the input type="date"
-var formattedDate = currentDate.toISOString().split('T')[0];
-console.log(formattedDate);
-
-// Set the value of the input field to the current date
-document.getElementById('reservation-date').value = formattedDate;
-//adjust
+// // Attach a single event listener to the parent element
+// strengthContainer.addEventListener('mouseover', handleHover);
+// strengthContainer.addEventListener('mouseout', handleHover);
+// strengthContainer.addEventListener('touchstart', handleHover);
+// strengthContainer.addEventListener('touchend', handleHover);
